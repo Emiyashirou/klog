063ae2543895043ac45a5aaa67ae832e10e897e7
lei.li <lei.li@slalom.com>
2018-10-19 22:40:15 -0500

    material components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component} from "react";
-import "./App.css";
+import React, { Component} from 'react';
+import './App.css';
+import ResponsiveDrawer from './dashboard/ResponsiveDrawer';
+import ComposeDialog from './dialog/ComposeDialog';
+import PostDialog from './dialog/PostDialog';
+import WorkPostList from './list/WorkPostList';
+import IdlePostList from './list/IdlePostList';
+import WorkCard from './feed/WorkCard';
+import LinearIndeterminate from './shared/LinearIndeterminate';
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <h1> Hello, World! </h1>
+      <div className='App'>
+        {/* <ResponsiveDrawer /> */}
+        {/* <ComposeDialog /> */}
+        {/* <PostDialog /> */}
+        {/* <WorkPostList /> */}
+        {/* <WorkCard /> */}
+        {/* <LinearIndeterminate /> */}
+        <IdlePostList />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: { presets: ['env'] }
+        options: { 
+          presets: ['@babel/env', '@babel/react'],
+          plugins: ['@babel/plugin-proposal-class-properties']
+        }
       },
       {
         test: /\.css$/,
@@ -20,14 +23,14 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: 'http://localhost:3000/dist/',
     hotOnly: true
   },
   plugins: [ new webpack.HotModuleReplacementPlugin() ]
